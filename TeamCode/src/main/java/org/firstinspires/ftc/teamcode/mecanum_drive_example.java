d695ddb1e2022753eb5b3fcdf29858118ebc46e6
CARY_ACADEMY\Felipe_Chiavegatto <felipe_chiavegatto@caryacademy.org>
2017-10-20 16:07:26 -0400

    Working Drive Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum_drive_example.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum_drive_example.java
@@ -1,23 +1,17 @@
 /*
 Copyright (c) 2016 Robert Atkinson
-
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted (subject to the limitations in the disclaimer below) provided that
 the following conditions are met:
-
 Redistributions of source code must retain the above copyright notice, this list
 of conditions and the following disclaimer.
-
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-
 Neither the name of Robert Atkinson nor the names of his contributors may be used to
 endorse or promote products derived from this software without specific prior
 written permission.
-
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
 LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -44,15 +38,16 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
  * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
+ * <p>
  * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
  * It includes all the skeletal structure that all linear OpModes contain.
- *
+ * <p>
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Mecanum Drive Example", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+@TeleOp(name = "Mecanum Drive Example", group = "Linear Opmode")
+// @Autonomous(...) is the other common choice
 
 public class mecanum_drive_example extends LinearOpMode {
 
@@ -75,7 +70,6 @@ public class mecanum_drive_example extends LinearOpMode {
         motorRightBack = hardwareMap.dcMotor.get("rightBack");
 
 
-
         // eg: Set the drive motor directions:
         // "Reverse" the motor that runs backwards when connected directly to the battery
         motorLeftFront.setDirection(DcMotor.Direction.FORWARD);
@@ -95,7 +89,7 @@ public class mecanum_drive_example extends LinearOpMode {
             //left stick translates
             //right stick rotates
             //this code is from https://ftcforum.usfirst.org/forum/ftc-technology/android-studio/6361-mecanum-wheels-drive-code-example.
-            //Some modifications made by Sheridan Page
+            //Some modifications made by Felipe Chiavegatto :)
 
             double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
             double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
@@ -109,7 +103,6 @@ public class mecanum_drive_example extends LinearOpMode {
             motorRightFront.setPower(v2);
             motorLeftBack.setPower(v3);
             motorRightBack.setPower(v4);
-
 
         }
     }
